32cd1045ae1b47c477c3ccdfb063a4bf0e130c30
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2024-01-05 18:15:59 +0900

    all next js learn finished

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -37,7 +36,6 @@ export async function fetchLatestInvoices() {
   noStore();
   try {
     console.log('Fetching invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
