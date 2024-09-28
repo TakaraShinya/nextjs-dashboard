11b8e245101cd6fa96969122f88cfff33becbe86
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2024-09-28 22:54:25 +0900

    add: ログ出力検証

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Customers desu',
-};
+// export const metadata: Metadata = {
+//   title: 'Customers desu',
+// };
+
+console.log("NODE_ENV = " + process.env.NODE_ENV);
+console.info("NODE_ENV = " + process.env.NODE_ENV);
+console.warn("NODE_ENV = " + process.env.NODE_ENV);
+console.error("NODE_ENV = " + process.env.NODE_ENV);
 
 export default function Page() {
     return <p>Customers Page</p>;
